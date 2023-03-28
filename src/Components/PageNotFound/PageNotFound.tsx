c85e47bf3ce052aaa1eda57a3e1bdb28567b9248
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-27 21:06:19 -0500

    Style the PageNotFound component

--- src/Components/PageNotFound/PageNotFound.tsx
+++ src/Components/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,11 @@
+import "./PageNotFound.css"
+
 export const PageNotFound = (): JSX.Element => {
   return (
-    <h2>404</h2>
+    <section className="page-not-found">
+      <h2>Oops! Page Not Found</h2>
+      <button className="page-not-found-btn">Return Home</button>
+      <img className="empty-closet-image" src="src/assets/empty-closet.png" alt="Picture of an empty closet"></img>
+    </section>
   )
 }
