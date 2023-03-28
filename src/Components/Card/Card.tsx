4304685188d0aef8f5b9a3e653e344e6a9f67eca
Blanche Haddad <blancherachel@gmail.com>
2023-03-28 10:35:22 -0600

    fix: Remove className

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Card.css";
 
 interface CardProps {
   id: number | string,
@@ -10,9 +11,9 @@ export const Card = ({ id, image }: CardProps): JSX.Element => {
   return (
     <div key={id} className="card-container">
       <Link to={`/api/v1/users/:id/items/${id}`}>
-        <img src={image} alt="Image of clothing item" className="card-image grow" />
+        <img src={image} alt="Image of clothing item" className="card-image" />
       </Link>
-        <p className="delete-banner">Delete Item</p>
+        <p className="delete-banner">Delete Item?</p>
     </div>
   )
 }
