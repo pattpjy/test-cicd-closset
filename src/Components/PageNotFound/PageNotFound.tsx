8b94687c53f70f39c33520a06e6349fe29158f91
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-27 21:08:10 -0500

    Add error handling when a user navigates to a page not found and add a back to home button

--- src/Components/PageNotFound/PageNotFound.tsx
+++ src/Components/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,14 @@
 import "./PageNotFound.css"
+import { NavLink } from "react-router-dom";
 
 export const PageNotFound = (): JSX.Element => {
   return (
     <section className="page-not-found">
-      <h2>Oops! Page Not Found</h2>
-      <button className="page-not-found-btn">Return Home</button>
+      <h2>Oops!</h2>
+      <p>404: Page Not Found</p>
+      <NavLink to={"/"}>
+        <button className="page-not-found-btn">Return Home</button>
+      </NavLink>
       <img className="empty-closet-image" src="src/assets/empty-closet.png" alt="Picture of an empty closet"></img>
     </section>
   )
