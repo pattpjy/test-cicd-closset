acafee65e98d405dc1d5a70f3fbbcafc8e8ffa5b
Karrar Qasim <Karawr1996@gmail.com>
2023-03-28 11:05:09 -0600

    Added the menuitems to home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,4 +7,15 @@ interface MenuItem {
   text: string;
   onClick: () => void;
  }
- 
+ export const AppMenu: React.FC = () => {
+  const logoSrc = images;
+  const menuItems: MenuItem[] = [
+    { iconSrc: images, text: 'My Closet', onClick: () => console.log('Clicked Menu Item 1') },
+    { iconSrc: images, text: 'My List', onClick: () => console.log('Clicked Menu Item 2') },
+    { iconSrc: images, text: 'Add New Item', onClick: () => console.log('Clicked Menu Item 3') },
+    { iconSrc: images, text: 'Create New List', onClick: () => console.log('Clicked Menu Item 4') },
+  ];
+  return (
+
+  );
+  };
