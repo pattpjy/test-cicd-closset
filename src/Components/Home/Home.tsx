011747178373763ce57e176be7da53cc12ae2256
Karrar Qasim <Karawr1996@gmail.com>
2023-03-28 16:18:59 -0600

    Added closet collection logo

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import images from './images.png'
+import Logoo from './Logoo.png'
 
 interface MenuItem {
   iconSrc: string;
@@ -13,12 +14,12 @@ interface MenuItem {
     { iconSrc: images, text: 'My Closet', onClick: () => console.log('Clicked Menu Item 1') },
     { iconSrc: images, text: 'My List', onClick: () => console.log('Clicked Menu Item 2') },
     { iconSrc: images, text: 'Add New Item', onClick: () => console.log('Clicked Menu Item 3') },
-    { iconSrc: images, text: 'Create New List', onClick: () => console.log('Clicked Menu Item 4') },
+    { iconSrc: images, text: 'Create New List', onClick: () => console.log('Clicked Menu Item 4') }
   ];
   return (
     <div className="container">
       <div className="logo-container">
-        <img className="logo" src={logoSrc} alt="Logo" />
+        <img className="logo" src={Logoo} alt="Logo" />
       </div>
       {menuItems.map(({ iconSrc, text, onClick }, index) => (
         <div className="menu-item" key={index} onClick={onClick}>
