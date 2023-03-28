e5f91abdeb1f2160221d5821faec9e2407a71ba8
Blanche Haddad <blancherachel@gmail.com>
2023-03-27 19:11:09 -0600

    feat: Add mappedItems and mappedItems in return

--- src/Components/Closet/Closet.tsx
+++ src/Components/Closet/Closet.tsx
@@ -1,5 +1,41 @@
-export const Closet = (): JSX.Element => {
+import { Card } from "../Card/Card";
+import "./Works.css";
+import { closetData } from "../../MockData/ClosetData.js";
+
+interface attributes {
+  season: string;
+  clothing_type: string;
+  size: string;
+  color: string;
+  image_url: string;
+  notes: string;
+}
+
+interface Item {
+  item: {
+    id: number, 
+    type: string,
+    attributes: attributes
+  }
+}
+
+interface ClosetProps {
+  item: Item[]
+}
+
+export const Closet: React.FC = ({ items: ClosetProps}): JSX.Element => {
+
+  const mappedItems = closetData.data.map(item => {
+    return 
+      <Card
+        id={item.id}
+        image={item.image_url}
+      />
+  })
+
   return (
-    <h2>Closet</h2>
+    <div className="cards-container">
+      {mappedItems}
+    </div>
   )
 }
