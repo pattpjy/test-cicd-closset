8f55ecb20a4e5a31b16af4148a497bf79452068d
Sarah Hampton <stoluba@gmail.com>
2023-03-27 21:09:14 -0400

    Create navbar with buttons and some basic styling

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css'
+
+
+export const Navbar = () => {
+
+  return(
+    <nav>
+      <button>home icon</button>
+      <button>my closet icon</button>
+      <button>my list icon</button>
+    </nav>
+  )
+}
