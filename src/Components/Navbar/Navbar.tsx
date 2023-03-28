76894f88361cf11aabd64f067bb058a175c6a23a
Sarah Hampton <stoluba@gmail.com>
2023-03-28 10:24:43 -0400

    Add Link to home button and add comments for buttons in navbar and navbar css

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Navbar.css'
 
 
-export const Navbar = () => {
+export function Navbar() {
 
-  return(
+  return (
     <nav>
-      <button className="nav-button">home icon</button>
+      <Link to={"/"}>
+        <button className="nav-button">home icon</button>
+      </Link>
+      {/* will need get requests for other buttons, 
+      need to discuss creating an apiCalls folder and having reusable requests */}
       <button className="nav-button">my closet icon</button>
       <button className="nav-button">my list icon</button>
     </nav>
-  )
+  );
 }
