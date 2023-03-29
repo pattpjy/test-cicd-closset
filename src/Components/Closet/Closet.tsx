afda7497c2bcdd8f929b5dcf872a58355ec9ed5a
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-28 20:47:10 -0500

    Create drop downs to filter items and console.log all of the selected option's values when any of the drop down values change

--- src/Components/Closet/Closet.tsx
+++ src/Components/Closet/Closet.tsx
@@ -36,14 +36,52 @@ export const Closet = ({ items }: ClosetProps): JSX.Element => {
     )
   })
 
-  const handleFilterButton = (): Item[] => {
-    console.log('filter by type!') //eventually this will be an API call
+  const handleFilter = (): void => {
+    const type = document.querySelector<HTMLSelectElement>("#filter--clothing-type")!
+    const color = document.querySelector<HTMLSelectElement>("#filter--color")!
+    const favorite = document.querySelector<HTMLSelectElement>("#filter--favorite")!
+    const season = document.querySelector<HTMLSelectElement>("#filter--season")!
+    console.log("Queries:", type.value, color.value, favorite.value, season.value) // these will be used for our queries
   }
 
   return (
     <div>
       <h2>My Closet</h2>
-      <button onClick={handleFilterButton} id='filter--att' className='filter-button'>Filter by Type</button>
+      <div id="filter" onChange={handleFilter}>
+        <select id="filter--clothing-type" name="type">
+          <option value="">Clothing Type</option>
+          <option value="tops">Tops</option>
+          <option value="bottoms">Bottoms</option>
+          <option value="outerwear">Outerwear</option>
+          <option value="shoes">Shoes</option>
+          <option value="accessories">Accessories</option>
+          <option value="other">Other</option>
+        </select>
+        <select id="filter--color" name="color">
+          <option value="">Color</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="purple">Purple</option>
+          <option value="black">Black</option>
+          <option value="white">White</option>
+          <option value="neutral">Neutral</option>
+          <option value="other">Multi</option>
+        </select>
+        <select id="filter--season" name="season">
+          <option value="">Season</option>
+          <option value="fall">Fall</option>
+          <option value="winter">Winter</option>
+          <option value="spring">Spring</option>
+          <option value="summer">Summer</option>
+        </select>
+        <select id="filter--favorite" name="favorite">
+          <option value="">See All</option>
+          <option value="favorites">Only Favorites</option>
+        </select>
+      </div>
       <div className='cards-container'>
         {mappedItems}
       </div>
