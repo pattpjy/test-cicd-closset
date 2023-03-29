c06c30c0a9c1b2b810c8a38fea630321c82a5ddd
Karrar Qasim <Karawr1996@gmail.com>
2023-03-28 18:29:33 -0600

    Added Links to appropriate buttons on landing page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
 import images from './images.png'
 import Logoo from './Logoo.png'
 
 interface MenuItem {
   iconSrc: string;
   text: string;
-  onClick: () => void;
- }
- export const AppMenu: React.FC = () => {
+  path: string;
+}
+
+export const AppMenu: React.FC = () => {
   const logoSrc = images;
   const menuItems: MenuItem[] = [
-    { iconSrc: images, text: 'My Closet', onClick: () => console.log('Clicked Menu Item 1') },
-    { iconSrc: images, text: 'My List', onClick: () => console.log('Clicked Menu Item 2') },
-    { iconSrc: images, text: 'Add New Item', onClick: () => console.log('Clicked Menu Item 3') },
-    { iconSrc: images, text: 'Create New List', onClick: () => console.log('Clicked Menu Item 4') }
+    { iconSrc: images, text: 'My Closet', path: '/api/v1/users/:id/items' },
+    { iconSrc: images, text: 'My List', path: '/api/v1/users/:id/lists' },
+    { iconSrc: images, text: 'Add New Item', path: '/api/v1/users/:id/items/new' },
+    { iconSrc: images, text: 'Create New List', path: '/api/v1/users/:id/lists/new' }
   ];
   return (
     <div className="container">
       <div className="logo-container">
         <img className="logo" src={Logoo} alt="Logo" />
       </div>
-      {menuItems.map(({ iconSrc, text, onClick }, index) => (
-        <div className="menu-item" key={index} onClick={onClick}>
+      {menuItems.map(({ iconSrc, text, path }, index) => (
+        <Link className="menu-item" key={index} to={path}>
           <img className="icon" src={iconSrc} alt="Menu Icon" />
           <div className="menu-text">{text}</div>
-        </div>
+        </Link>
       ))}
     </div>
   );
- };
- 
+};
