81e1826933b62660c46c42bd024fa42519448a0a
Karrar Qasim <Karawr1996@gmail.com>
2023-03-28 21:02:07 -0600

    Removed pictures from menu options

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,30 +1,27 @@
 import './Home.css';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import images from './images.png'
-import Logoo from './Logoo.png'
+import Logoo from './Logoo.png';
 
 interface MenuItem {
-  iconSrc: string;
   text: string;
   path: string;
 }
 
 export const AppMenu: React.FC = () => {
   const menuItems: MenuItem[] = [
-    { iconSrc: images, text: 'My Closet', path: '/api/v1/users/:id/items' },
-    { iconSrc: images, text: 'My List', path: '/api/v1/users/:id/lists' },
-    { iconSrc: images, text: 'Add New Item', path: '/api/v1/users/:id/items/new' },
-    { iconSrc: images, text: 'Create New List', path: '/api/v1/users/:id/lists/new' }
+    { text: 'My Closet', path: '/api/v1/users/:id/items' },
+    { text: 'My List', path: '/api/v1/users/:id/lists' },
+    { text: 'Add New Item', path: '/api/v1/users/:id/items/new' },
+    { text: 'Create New List', path: '/api/v1/users/:id/lists/new' }
   ];
   return (
     <div className="container">
       <div className="logo-container">
         <img className="logo" src={Logoo} alt="Logo" />
       </div>
-      {menuItems.map(({ iconSrc, text, path }, index) => (
+      {menuItems.map(({ text, path }, index) => (
         <Link className="menu-item" key={index} to={path}>
-          <img className="icon" src={iconSrc} alt="Menu Icon" />
           <div className="menu-text">{text}</div>
         </Link>
       ))}
