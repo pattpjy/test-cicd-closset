708324c2b9dfb302a409b8afea1b25e1f61cba47
Blanche Haddad <blancherachel@gmail.com>
2023-03-29 17:40:34 -0600

    fix: Render Header for all views

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
-import { Route, Routes } from "react-router"
-import { AppMenu } from "../Home/Home"
-import { PageNotFound } from "../PageNotFound/PageNotFound"
-import { Closet } from "../Closet/Closet"
-import { Details } from "../Details/Details"
-import { MyLists } from "../MyLists/MyLists"
-import { List } from "../List/List"
-import { AddItem } from '../Add Item/AddItem'
+import { Route, Routes } from "react-router";
+import { AppMenu } from "../Home/Home";
+import { PageNotFound } from "../PageNotFound/PageNotFound";
+import { Closet } from "../Closet/Closet";
+import { Details } from "../Details/Details";
+import { MyLists } from "../MyLists/MyLists";
+import { List } from "../List/List";
+import { AddItem } from '../AddItem/AddItem';
+import { Header } from '../Header/Header';
 
 function App() {
  
   return (
     <main>
+      <Header />
       <Routes>
         <Route path="/" element={<AppMenu />} />
         <Route path="/*" element={<PageNotFound />} />
