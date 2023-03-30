521682b3c0a26feeadef83b25df0ac68f32f5e8d
Karrar Qasim <Karawr1996@gmail.com>
2023-03-29 21:01:09 -0600

    Added fetch to my list component

--- src/Components/MyLists/MyLists.tsx
+++ src/Components/MyLists/MyLists.tsx
@@ -9,6 +9,15 @@ interface ListItem {
 export const MyLists: React.FC<{ userId: number }> = ({ userId }) => {
   const [lists, setLists] = useState<ListItem[]>([]);
 
+  useEffect(() => {
+    const fetchLists = async () => {
+      const response = await fetch(`https://closet-manager-be.herokuapp.com/api/v1/users/${userId}/lists`);
+      const data = await response.json();
+      setLists(data);
+    };
+    fetchLists();
+  }, [userId]);
+
   return (
 
   );
