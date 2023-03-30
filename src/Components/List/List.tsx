dca5c7cd43447b2f27ff0413db77e924bebe7d12
Patt Sookmark <pijiya@sookmark.com>
2023-03-30 16:55:08 -0600

    Add create list function and apiCall

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,22 +1,55 @@
 import React from "react";
 import { Header } from "../Header/Header";
 import { Navbar } from "../Navbar/Navbar";
-// import API from "../adapters/API";
+import { useState } from "react";
 
 export const List: React.FC = (): JSX.Element => {
+  const [newCustomList, setNewCustomList] = useState<string | null>(null);
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  //apiCall Function  these need to move to apiCall file
+  const postCustomList = async (data: any) => {
+    // we cannot leave data: any!! We should make an interface
+    const url = ``;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error("Unable To Fetch Your Data. Try Later.");
+    }
+    return response.json();
+  };
+
+  const createCustomList = async () => {
+    let errorMsg = (value: string): any => {
+      throw new Error(value);
+    };
+
+    try {
+    } catch (Error) {
+      setHasError(true);
+    }
+  };
+
+  const onClickHandler = () => {
+    createCustomList();
+  };
   return (
     <div className="form-container">
       <h2 className="form-title">Add My New Custom List</h2>
       <form className="form">
-        <label htmlFor="caption" className="size-input">
+        <label htmlFor="caption" className="custom-list-input">
           My custom list Name
           <input type="text" name="custom-name" />
         </label>
-        <label htmlFor="caption" className="notes-input">
-          Notes
-          <input className="notes-box" type="text" name="notes" />
-        </label>
-        <button type="submit" value="Submit" className="form-button">
+        <button
+          type="submit"
+          value="Submit"
+          className="form-button"
+          onClick={onClickHandler}
+        >
           Add My Custom List
         </button>
       </form>
