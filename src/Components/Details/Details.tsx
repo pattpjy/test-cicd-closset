9641f9779916d670367145c4e142e69b9bfde5f8
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-31 17:01:07 -0500

    Redirect user to the item not found page if a fetch fails and conditionally render a loading message

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -3,7 +3,7 @@ import "./Details.css";
 import { useParams } from "react-router-dom";
 import { NavLink, useNavigate } from "react-router-dom";
 import { getSingleItem } from "../../apiCall"
-
+import { singleItemCleaning } from "../../util"
 
 interface attributes {
   season: string;
@@ -28,22 +28,41 @@ export const Details = (): JSX.Element => {
   const [loading, setLoading] = useState<boolean>(true); 
 
   useEffect(() => {
-   console.log("params", params.id)
    getSingleItem(params.id!)
    .then((response) => {
-    console.log(response)
-        setItem(response.data)
+        setItem(singleItemCleaning(response.data))
         setFetchError(false)
         setLoading(false)
       })
       .catch((Error)  => {
-        console.log("Item Fetch Error")
-        setItem(undefined)
         setFetchError(true)
-        setLoading(false)
       })
   }, []);
+
+   useEffect(() => {
+    if (fetchError) {
+      navigate('/item-not-found')
+    }
+  }, [fetchError]);
+
   return (
-    <h2>Details</h2>
+    <section className="details-section">
+      <h2 className="item-details-header">Details</h2>
+      {loading && <p>Loading...</p>}
+      {item && <div className="item-details-container">
+        <p className="item-details">{`${item!.attributes.color}`}</p>
+        <p className="item-details">{`${item!.attributes.season}`}</p>
+        <p className="item-details">{`${item!.attributes.clothing_type}`}</p>
+      </div>}
+      {item && <img className="details-image" src={item.attributes.image_url} alt='Image of clothing item'/> }
+      {item && item!.attributes.notes && <p className="item-notes">{item!.attributes.notes}</p>}
+      {!loading && <section className="details-button-container">
+        <NavLink to={`/edit/${params.id}`}>
+          <button className="details-edit-button">Edit</button>
+        </NavLink>
+        <button className="details-list-button">Add to List</button>
+        <button className="details-delete-button">Delete</button>
+      </section>}
+    </section>
   )
 }
