c67785dba004db2e552211eb4694608e3c095e96
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-31 16:35:29 -0500

    Create component for when an item is not found

---
+++ src/Components/ItemNotFound/ItemNotFound.tsx
@@ -0,0 +1,14 @@
+import "./ItemNotFound.css";
+import { NavLink } from "react-router-dom";
+
+export const ItemNotFound = (): JSX.Element => {
+  return (
+     <section className="item-not-found">
+      <p>Error: No Item Details</p>
+      <NavLink to={"/"}>
+        <button className="item-not-found-btn">Return Home</button>
+      </NavLink>
+      <img className="empty-closet-image" src="src/assets/empty-closet.png" alt="Picture of an empty closet"></img>
+    </section>
+  );
+};
