9394685b6ffbc4e447f6260110ed541646f30757
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-03-31 16:35:03 -0500

    Create cleaning function for single items

---
+++ src/util.tsx
@@ -0,0 +1,24 @@
+interface attributes {
+  season: string;
+  clothing_type: string;
+  size: string;
+  color: string;
+  image_url: string;
+  notes: string;
+}
+
+interface Item {
+  id: number;
+  type: string;
+  attributes: attributes;
+}
+
+export const singleItemCleaning = (item: Item): Item => {
+ if (item!.attributes.season === "all_season") {
+  item!.attributes.season = "all seasons"
+ }
+ if (item!.attributes.notes === "empty again" || item!.attributes.notes === null) {
+  item!.attributes.notes = ""
+ }
+ return item
+}
