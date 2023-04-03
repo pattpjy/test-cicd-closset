b77c521d6b5cd6b07a22bec1b99c289a36b4ecf5
Patt Sookmark <pijiya@sookmark.com>
2023-04-03 14:17:43 -0600

    Add cypress testing for addlist view

--- cypress/e2e/addlist.cy.ts
+++ cypress/e2e/addlist.cy.ts
@@ -1,17 +1,44 @@
 describe("AddList Page", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:5173/addlist");
-  });
   it("should render correctly", () => {
+    cy.visit("http://localhost:5173/addlist");
     //have logo
     //have navbar
-    //have header
     //have input box with placeholder
     //have button for submit
+
+    cy.get(".logo-img").should("be.visible");
+    cy.get("#nav-bar").should("be.visible");
+    cy.get(".form--list").should("be.visible");
+    cy.get(".form-button").should("be.visible");
+    cy.get("#root > main > div > form > label > input[type=text]").should(
+      "have.attr",
+      "placeholder"
+    );
+    cy.get('[placeholder="Add Custom List Name"]').should("be.visible");
   });
-  it("should submit the form correctly", () => {
-    //
+
+  describe("form submission", () => {
+    it("should display the form with the correct input", () => {
+      cy.visit("http://localhost:5173/addlist");
+      cy.get("#root > main > div > form > label > input[type=text]")
+        .type("Hawaii Trip")
+        .should("have.value", "Hawaii Trip");
+    });
+    it("should submit the form correctly", () => {
+      cy.visit("http://localhost:5173/addlist");
+      //intercept and fixture
+      cy.intercept(
+        {
+          method: "POST",
+          url: "https://closet-manager-be.herokuapp.com/api/v1/users/1/lists",
+        },
+        { name: "Random place to visit" }
+      );
+      cy.get("#root > main > div > form > label > input[type=text]").type(
+        "Random place to visit"
+      );
+      cy.get("#root > main > div > form > button").click();
+      cy.get(".alert-msg").should("contain", "YOUR CUSTOM LIST IS CREATED");
+    });
   });
-  it("should have response with message when form is a successful post");
-  it("should have response with message when form is an unsuccessful post");
 });
