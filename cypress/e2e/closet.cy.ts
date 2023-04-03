43a43895b0f40109f72c1bbdd179ead7383c65b0
Blanche Haddad <blancherachel@gmail.com>
2023-04-03 11:47:02 -0600

    feat: Add beginning of tests for Closet view

---
+++ cypress/e2e/closet.cy.ts
@@ -0,0 +1,19 @@
+describe("Closet View", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items", {fixture: "closet"})
+    cy.visit("http://localhost:5173/");
+  })
+  it("Should be able to navigate to the closet from the landing page", () => {
+    cy.get('.home-container > [href="/myCloset"]').click();
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain("/myCloset");
+    });
+  });
+  it("Should contain cards of images with a banner that says 'Delete'", () => {
+    cy.get('.home-container > [href="/myCloset"]').click();
+    cy.get(".card-image").should("have.length", 3).should("be.visible");
+    cy.get(".banner-container").should("have.length", 3).should("be.visible");
+    cy.get(':nth-child(1) > a > .card-image').should("be.visible");
+  });
+  it("Should be able to delete an item from the closet")
+});
