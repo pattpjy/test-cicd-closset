cd439dff4202110edad64fdfdf3f728013309ee7
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-03 15:39:45 -0500

    Add delete functionality to the delete button on the details page

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -2,7 +2,7 @@ import "./Details.css";
 import { useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 import { NavLink, useNavigate } from "react-router-dom";
-import { getSingleItem } from "../../apiCall";
+import { getSingleItem, deleteItem } from "../../apiCall";
 import { singleItemCleaning } from "../../util";
 
 interface attributes {
@@ -20,21 +20,26 @@ interface Item {
   attributes: attributes;
 }
 
+type IdParams = {
+  id: string;
+}
+
 export const Details = (): JSX.Element => {
-  const params = useParams();
+  const { id } = useParams<IdParams>();
   const navigate = useNavigate();
   const [item, setItem] = useState<Item | undefined>();
   const [fetchError, setFetchError] = useState<boolean>(false); 
   const [loading, setLoading] = useState<boolean>(true); 
+  const [isDeleted, setIsDeleted] = useState<boolean>(false); 
 
   useEffect(() => {
-   getSingleItem(params.id!)
+   getSingleItem(id!)
    .then((response) => {
         setItem(singleItemCleaning(response.data))
         setFetchError(false)
         setLoading(false)
       })
-      .catch((Error)  => {
+      .catch(()  => {
         setFetchError(true)
       })
   }, []);
@@ -43,23 +48,41 @@ export const Details = (): JSX.Element => {
     if (fetchError) {
       navigate('/item-not-found')
     }
-  }, [fetchError]);
+    if (isDeleted) {
+      setItem(undefined)
+      setTimeout(() => {
+        navigate('/myCloset')
+      }, 3500)
+    }
+  }, [fetchError, isDeleted]);
+
+  const handleDelete = (id: string) =>  {
+    deleteItem(id)
+     .then((response) => {
+        setIsDeleted(true)
+      })
+    .catch(()  => {
+        setFetchError(true)
+    })
+  }
 
   return (
     <section className="details-section">
       <h2 className="item-details-header">Details</h2>
       {loading && <p>Loading...</p>}
+      {isDeleted && <p>Success! This item has been removed from your closet.</p>}
+      {isDeleted && <p>Redirecting ...</p>}
       {item && <div className="item-details-container">
-        <NavLink to={`/edit/${params.id}`}>
+        <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.color}</p>
         </NavLink>
-        <NavLink to={`/edit/${params.id}`}>
+        <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.season}</p>
         </NavLink>
-        <NavLink to={`/edit/${params.id}`}>
+        <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.clothing_type}</p>
         </NavLink>
-        {item!.attributes.size && <NavLink to={`/edit/${params.id}`}>
+        {item!.attributes.size && <NavLink to={`/edit/${id}`}>
           <p className="item-details">{`size ${item!.attributes.size}`}</p>
         </NavLink>}
       </div>}
@@ -68,13 +91,13 @@ export const Details = (): JSX.Element => {
         <h2 className="item-notes-header">Notes</h2>
         <p className="item-notes">{item!.attributes.notes}</p> 
         </div>}
-      {!loading && <section className="details-button-container">
-        <NavLink to={`/edit/${params.id}`}>
+      {!loading && <div className="details-button-container">
+        <NavLink to={`/edit/${id}`}>
           <button className="details-edit-button">Edit</button>
         </NavLink>
         <button className="details-list-button">Add to List</button>
-        <button className="details-delete-button">Delete</button>
-      </section>}
+        <button className="details-delete-button" onClick={() => handleDelete(id!)}>Delete</button>
+      </div>}
     </section>
   )
 }
