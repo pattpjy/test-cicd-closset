e23a06493fe99b94d37787824910b0e8c7ad626b
Blanche Haddad <blancherachel@gmail.com>
2023-04-03 14:16:41 -0600

    feat: Finish testing for Closet

--- cypress/e2e/closet.cy.ts
+++ cypress/e2e/closet.cy.ts
@@ -11,12 +11,19 @@ describe("Closet View", () => {
   });
   it("Should contain cards of images with a banner that says 'Delete'", () => {
     cy.get('.home-container > [href="/myCloset"]').click();
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain("/myCloset");
+    });
     cy.get(".card-image").should("have.length", 3).should("be.visible");
     cy.get(".banner-container").should("have.length", 3).should("be.visible");
     cy.get(':nth-child(1) > a > .card-image').should("be.visible");
   });
   it("Should be able to delete an item from the closet", () => {
-    cy.get(".banner-container").first().click();
-    cy.get(".card-image").should("have.length", 2).should("be.visible");
+    cy.intercept("DELETE", "https://closet-manager-be.herokuapp.com/api/v1/users/1/items/1")
+    cy.get('.home-container > [href="/myCloset"]').click();
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain("/myCloset");
+    });
+    cy.get(':nth-child(1) > .banner-container > .delete-banner').click();
   })
 });
