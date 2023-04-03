8308a380ac835333c7480a8092ee540aeb978ef5
Patt Sookmark <pijiya@sookmark.com>
2023-04-03 12:07:38 -0600

    Add beforeEach

---
+++ cypress/e2e/addlist.cy.ts
@@ -0,0 +1,17 @@
+describe("AddList Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/addlist");
+  });
+  it("should render correctly", () => {
+    //have logo
+    //have navbar
+    //have header
+    //have input box with placeholder
+    //have button for submit
+  });
+  it("should submit the form correctly", () => {
+    //
+  });
+  it("should have response with message when form is a successful post");
+  it("should have response with message when form is an unsuccessful post");
+});
