1268fc03986dd61105f7e30df4b812298c675660
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-04 09:01:53 -0500

    Conditionally render the item's color and disable buttons after a user deleted an item

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -1,7 +1,6 @@
 import "./Details.css";
 import { useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
 import { getSingleItem, deleteItem } from "../../apiCall";
 import { singleItemCleaning } from "../../util";
 
@@ -58,7 +57,7 @@ export const Details = (): JSX.Element => {
 
   const handleDelete = (id: string) =>  {
     deleteItem(id)
-     .then((response) => {
+     .then(() => {
         setIsDeleted(true)
       })
     .catch(()  => {
@@ -70,18 +69,18 @@ export const Details = (): JSX.Element => {
     <section className="details-section">
       <h2 className="item-details-header">Details</h2>
       {loading && <p>Loading...</p>}
-      {isDeleted && <p>Success! This item has been removed from your closet.</p>}
+      {isDeleted && <p>This item has been removed from your closet.</p>}
       {isDeleted && <p>Redirecting ...</p>}
       {item && <div className="item-details-container">
-        <NavLink to={`/edit/${id}`}>
+        {item!.attributes.color && <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.color}</p>
-        </NavLink>
+        </NavLink>}
         <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.season}</p>
         </NavLink>
-        <NavLink to={`/edit/${id}`}>
+        {item!.attributes.clothing_type && <NavLink to={`/edit/${id}`}>
           <p className="item-details">{item!.attributes.clothing_type}</p>
-        </NavLink>
+        </NavLink>}
         {item!.attributes.size && <NavLink to={`/edit/${id}`}>
           <p className="item-details">{`size ${item!.attributes.size}`}</p>
         </NavLink>}
@@ -91,9 +90,9 @@ export const Details = (): JSX.Element => {
         <h2 className="item-notes-header">Notes</h2>
         <p className="item-notes">{item!.attributes.notes}</p> 
         </div>}
-      {!loading && <div className="details-button-container">
+      {!loading && !isDeleted && <div className="details-button-container">
         <NavLink to={`/edit/${id}`}>
-          <button className="details-edit-button">Edit</button>
+            <button className="details-edit-button">Edit</button>
         </NavLink>
         <button className="details-list-button">Add to List</button>
         <button className="details-delete-button" onClick={() => handleDelete(id!)}>Delete</button>
