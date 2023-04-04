c46b99a70b060b04fc542a2156f0f235261e669d
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-04-04 12:15:12 -0500

    Add Data interface

--- src/apiCall.ts
+++ src/apiCall.ts
@@ -1,6 +1,17 @@
+interface Data {
+  data: {
+    item: {
+      season: string; 
+      color: string;
+      clothing_type: string;
+      size: string;
+      notes: string;
+    }
+  } 
+}
+
 export const getAllItems = async () => {
   const url = "https://closet-manager-be.herokuapp.com/api/v1/users/1/items";
-  // hard-coding this fetch for user 1 for now, make dynamic if we add other users
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Unable To Fetch Your Data. Try Later.");
@@ -16,18 +27,11 @@ export const filterItems = async (url: string) => {
   return response.json()
 };
 
-export const createItem = async (data: any) => {
-  // we cannot leave data: any!! We should make an interface 
+export const createItem = async (data: FormData) => {
   const url = `https://closet-manager-be.herokuapp.com/api/v1/users/1/items`;
-    // hard-coding this fetch for user 1 for now, make dynamic if we add other users
   const response = await fetch(url, {
     method: "POST",
-    // "There is no "Content-Type" key in the headers — the 
-    // content type is multipart/form-data,
-    //  which is implied by the FormData object itself"
     body: data,
-    // "The body is not stringified. The FormData API handles all 
-    // the necessary processing for the image to be sent over the web."
   });
   if (!response.ok) {
     console.log(response)
@@ -36,17 +40,14 @@ export const createItem = async (data: any) => {
   return response.json();
 };
 
-export const editItem = async ({data}: any , id: number) => {
-
-  // we cannot leave data: any!! We should make an interface 
+export const editItem = async ({data}: Data , id: number) => {
   const url = `https://closet-manager-be.herokuapp.com/api/v1/users/1/items/${id}`;
-    // hard-coding this fetch for user 1 for now, make dynamic if we add other users
   const response = await fetch(url, {
     method: "PATCH",
     headers: {
     "Content-Type": "application/json"
   },
-    body:  JSON.stringify(data) ,
+    body:  JSON.stringify(data),
   });
   if (!response.ok) {
     console.log(response)
